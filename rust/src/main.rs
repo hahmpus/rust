aab32066cbfd28810e82a1c62e8f303a4f28a905
Hahmpus <hampus@wondr.cc>
2022-05-05 13:04:35 +0200

    docker start, app structure

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -20,11 +20,11 @@ async fn main() -> std::io::Result<()> {
 
         App::new()
             .wrap(logger)
-            .default_service(Files::new("", "react/build"))
+            .default_service(Files::new("", "../react/build"))
             .service(get_product)
 
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
